8582e7dd947f992acc8ef5f5bcc6b828bea650ac
ronaldnino45 <ronaldninosuarez@gmail.com>
2025-11-07 12:10:12 -0500

    crud2

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # Import libraries
+from flask import Flask, url_for, redirect, render_template
 
 # Instantiate Flask functionality
+app = Flask("_name_")
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
 
